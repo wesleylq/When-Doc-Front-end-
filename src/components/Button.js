2be9534dcbb4d7c97943f1f4f565cc1d3d8f46f5
lucasduarte2 <lucas.duarte@ccc.ufcg.edu.br>
2018-09-28 20:09:49 -0300

    adicionado componets de button e input

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = (props) => {
+  return (
+  		<button type="button" className="btn btn-primary">{props.name}</button>
+    );
+}
+
+export default Button;
+
+
