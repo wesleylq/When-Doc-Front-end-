70624e6c43987ce0261085128681b2290444be62
lucasduarte2 <lucas.duarte@ccc.ufcg.edu.br>
2018-10-16 19:04:35 -0300

    adicao do lista e cadastar paciente

---
+++ src/paciente/ModalCadastraPaciente.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Container, Row, Col, Input, Button, Fa, Modal, ModalBody, ModalHeader, ModalFooter } from 'mdbreact';
+import axios from 'axios';
+
+ class ModalCadastraPaciente extends Component {
+ constructor(props) {
+  super(props);
+  this.state = {
+    modal: false
+  }
+  this.toggle = this.toggle.bind(this);
+}
+
+handleSubmit = event => {
+  const user = {
+    nome: this.state.nome,
+    data: this.state.data,
+    cpf: this.state.cpf
+
+  }
+
+  axios.post(`http://localhost:3000/paciente`,  user )
+  .then(res => {
+    console.log(res);
+    console.log(res.data);
+  })
+}
+
+
+toggle() {
+  this.setState({
+    modal: !this.state.modal
+  });
+}
+
+
+render() {
+  return (
+    <div>
+      <Button onClick={this.toggle}>add paciente</Button>
+      <Modal isOpen={this.state.modal} toggle={this.toggle}>
+        <ModalBody>
+          <Col md="12">
+            <form onSubmit={this.handleSubmit}>
+              <p className="h5 text-center">Cadastrar Paciente</p>
+              <div className="grey-text">
+                <Input label="nome" onChange={(value) => this.setState({nome: value.target.value})} className="form-control is-valid" required/>
+                <Input onChange={(value) => this.setState({data: value.target.value})} className="form-control is-valid" required/>
+                <Input onChange={(value) => this.setState({cpf: value.target.value})} className="form-control is-valid" required/>
+                <Button color="mdb-color" type="submit" rounded className="float-right">Cadastar</Button>
+                <Button color="secondary" onClick={this.toggle} className="float-right">Fechar</Button>{' '}
+              </div>
+            </form>
+          </Col>
+        </ModalBody>
+      </Modal>
+    </div>
+    )
+  }
+}
+export default ModalCadastraPaciente
---
+++ src/paciente/Paciente.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { MDBDataTable } from 'mdbreact';
+import './Paciente.css'
+import { Table, TableBody, TableHead  } from 'mdbreact'
+import ModalCadastraPaciente from './ModalCadastraPaciente.js'
+class Paciente extends Component {
+
+    state = {
+        pacientes: []
+    }
+    componentDidMount() {
+        axios.get(`http://localhost:3000/paciente`)
+        .then(res => {
+            const pacientes = res.data;
+            this.setState({ pacientes });
+        })
+    }
+
+      render() {
+        return (
+            <div> 
+               <ModalCadastraPaciente/>
+                <Table striped bordered hover id="con">
+                    <TableHead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Data Nascimento</th>
+                            <th>CPF</th>
+                        </tr>
+                    </TableHead>
+                    <TableBody>
+                       { this.state.pacientes.map(paciente =>
+                            <tr> 
+                                <td>{paciente.nome}</td>
+                                <td>{paciente.data}</td>
+                                <td>{paciente.cpf}</td>
+                            </tr>)}
+                    </TableBody>
+                </Table>
+            </div>
+        );
+    }
+}
+
+export default Paciente
