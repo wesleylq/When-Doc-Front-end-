922d5d267b867cd867889c8ac9fc8ae52ed64fba
lucasduarte2 <lucas.duarte@ccc.ufcg.edu.br>
2018-10-22 09:16:52 -0300

    correcao da router

--- src/menu/NavBar.js
+++ src/menu/NavBar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './NavBar.css'
-
+import { Link, Switch, Route } from 'react-router-dom';
 class NavBar extends React.Component {
     constructor(props) {
         super(props);
@@ -20,29 +20,27 @@ class NavBar extends React.Component {
     }
     render() {
         return (
-            <Router>
-                <Navbar id='navbar' dark expand="md" scrolling>
-                    <NavbarBrand href="/menu">
-                        <i className="fa fa-heartbeat" aria-hidden="true"></i> <strong> When,Doc</strong>
-                    </NavbarBrand>
-                    { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
-                    <Collapse isOpen = { this.state.collapse } navbar>
-                        <NavbarNav left id="navbar">
-                          <NavItem>
-                              <NavLink to="#">Meu Perfil</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <NavLink className="nav-link" to="/paciente">Pacientes</NavLink>
-                          </NavItem>
-                        </NavbarNav>
-                        <NavbarNav right id="navbar">
-                          <NavItem>
-                            <NavLink to="logaut">Sair</NavLink>
-                          </NavItem>
-                        </NavbarNav>
-                    </Collapse>
-                </Navbar>
-            </Router>
+            <Navbar id='navbar' dark expand="md" scrolling>
+                <NavbarBrand href="/menu">
+                    <i className="fa fa-heartbeat" aria-hidden="true"></i> <strong> When,Doc</strong>
+                </NavbarBrand>
+                { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
+                <Collapse isOpen = { this.state.collapse } navbar>
+                    <NavbarNav left id="navbar">
+                      <NavItem>
+                          <NavLink to="#">Meu Perfil</NavLink>
+                      </NavItem>
+                      <NavItem>
+                          <Link className="nav-link" to="/paciente">Pacientes</Link>
+                      </NavItem>
+                    </NavbarNav>
+                    <NavbarNav right id="navbar">
+                      <NavItem>
+                        <NavLink to="logaut">Sair</NavLink>
+                      </NavItem>
+                    </NavbarNav>
+                </Collapse>
+            </Navbar>
         );
     }
 }
