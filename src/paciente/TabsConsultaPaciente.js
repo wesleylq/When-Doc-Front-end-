3c286ddb4c61d0d35c801d4cc67e47391bd412e7
lucasduarte2 <lucas.duarte@ccc.ufcg.edu.br>
2018-10-26 13:57:52 -0300

    adicionado historico do paciente

--- src/paciente/TabsConsultaPaciente.js
+++ src/paciente/TabsConsultaPaciente.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Perfil from './perfil/Perfil.js'
+import Historico from './historico/Historico.js'
+
 const TabsConsultaPaciente = (props) => {
   return (
 	  	<div>
---
+++ src/paciente/historico/Historico.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Historico.css'
+
+const Historico = (props) => {
+  return (
+      <section id='historicoPaciente'>
+
+      </section>
+  );
+}
+
+export default Historico;
