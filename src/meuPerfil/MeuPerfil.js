424c5afffb985469d1901a4d298fb18365c83d7f
lucasduarte2 <lucas.duarte@ccc.ufcg.edu.br>
2018-11-02 11:30:45 -0300

    add modal de edit info medico

--- src/meuPerfil/MeuPerfil.js
+++ src/meuPerfil/MeuPerfil.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import './MeuPerfil.css'
-
+import './MeuPerfil.css';
+import EditDados from './EditDados.js';
 const MeuPerfil = (props) => {
   return (
       <section className="team-section text-center my-5">
-          <div class="jumbotron text-center">
-              <h2 class="card-title h2">Lucas venancio Duarte</h2>
-              <p class="blue-text my-4 font-weight-bold">espericalidade</p>
-              <div class="row d-flex justify-content-center">
-                <div class="col-xl-7 pb-2">
-                  <p class="card-text">CRM</p>
-                  <p class="card-text">Cpf</p>
-                </div>
+        <div className="jumbotron text-center">
+            <EditDados/>
+            <h2 className="card-title h2">Lucas venancio Duarte</h2>
+            <p className="blue-text my-4 font-weight-bold">espericalidade</p>
+            <div className="row d-flex justify-content-center">
+              <div className="col-xl-7 pb-2">
+                <p className="card-text">CRM</p>
+                <p className="card-text">Cpf</p>
               </div>
-              <hr class="my-4"/>
-              <div class="pt-1">
-                <p class="card-text">Email</p>
-                <p class="card-text">Telefone</p>
-              </div>
-           </div>
+            </div>
+            <hr className="my-4"/>
+            <div className="pt-1">
+              <p className="card-text">Email</p>
+              <p className="card-text">Telefone</p>
+            </div>
+         </div>
       </section>
   );
 }
