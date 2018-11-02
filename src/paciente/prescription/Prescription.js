4abac69cbe51fbc3baa71acd991b5da2a5c51e3c
Wesley Lucena Queiroz <wesley.lucena.queiroz@ccc.ufcg.edu.br>
2018-11-02 14:21:45 -0300

    Receituário finalizado

--- src/paciente/prescription/Prescription.js
+++ src/paciente/prescription/Prescription.js
@@ -1,33 +1,53 @@
 import React from 'react';
-import { Card,CardTitle,Input, FormInline,Button,Container, Row, Col } from 'mdbreact';
+import { Card,Button } from 'mdbreact';
 import "./Prescription.css"
+import { withRouter } from "react-router-dom";
 
 class Prescription extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            
-         };
+        this.state = {
+            meds: []           
+         };   
+              
     }
+   
 
-    add = () => {
+    handleAdd = () => {
         const medicamento = {
             nome: this.state.nome,
             descricao: this.state.descricao,
             dosagem: this.state.dosagem,
-            data: this.state.data,
+            intervalo: this.state.intervalo,
             quantidade: this.state.quantidade
             
           }
-        
+          this.setState({
+            meds: [...this.state.meds, medicamento]
+          });
+          this.myFormRef.reset();
       }
+
+      handleRemove(med){        
+        var array = this.state.meds.filter(function(s) { return s != med });
+        this.setState({meds: array });        
+     }
+
+     handlePrescription(){        
+        this.setState({meds: [] });    
+        window.location.reload();
+     }
+
+     
+
+   
     render() {
         return (
           <Card>
             <div className="container ">            
             <div className="row">
                 <div className="col-sm-12 ">
-                <div className= "card-body"><legend align="center">Nome do Paciente</legend></div>                                               
+                <div className= "card-body"><legend align="center">{this.props.nome}</legend></div>                                               
                             <div className="tab_container">
                                 <input id="tab1" type="radio" name="tabs" defaultChecked/>
                                 <label id="tab" htmlFor="tab1"><i className="test fa fa-code"></i><span>Diagnosticar</span></label>
@@ -54,16 +74,17 @@ class Prescription extends React.Component {
 
                                 <section id="content2" className="tab_content">
                                 <div className="row">
-                                <div className="col-sm-5">
+                                <div className="col-sm-6">
                                         
                                         <div className="panel panel-default">
                                             <div className="panel-body form-horizontal">
+                                            <form ref={(el) => this.myFormRef = el}>
                                                 <div className="form-group">
                                                     <label htmlFor="concept" className="control-label">Medicamento</label>
                                                     <div>
                                                         <input type="text" className="form-control"
                                                             onChange={(value) => this.setState({nome: value.target.value})}/>
-                                                    </div>
+                                                    </div>                                                
                                                 </div>
                                                 <div className="form-group">
                                                     <label htmlFor="description" className="control-label">Descrição</label>
@@ -73,60 +94,69 @@ class Prescription extends React.Component {
                                                     </div>
                                                 </div>
                                                 <div className="form-group">
+                                                    <label htmlFor="amount" className=" col-sm-5 control-label">Dosagem (mg)</label>
+                                                    <div className="col-sm-5">
+                                                        <input type="number" className="form-control" id="dose" name="dose" 
+                                                         onChange={(value) => this.setState({dosagem: value.target.value})}/>
+                                                    </div>
+                                                </div>
+                                                <div className="form-group">
                                                     <label htmlFor="amount" className=" col-sm-5 control-label">Quantidade</label>
-                                                    <div className="col-sm-9">
+                                                    <div className="col-sm-5">
                                                         <input type="number" className="form-control" id="amount" name="amount" 
                                                          onChange={(value) => this.setState({quantidade: value.target.value})}/>
                                                     </div>
                                                 </div>
+                                                
                                                 <div className="form-group">
-                                                    <label htmlFor="status" className="col-sm-5 control-label">Dosagem</label>
-                                                       <div className="col-sm-9">
-                                                        <input type="date" className="form-control" id="date" name="date" 
-                                                         onChange={(value) => this.setState({dosagem: value.target.value})}/>                                                    
+                                                    <label htmlFor="amount" className=" col-sm-5 control-label">Intervalo (hora)</label>
+                                                    <div className="col-sm-5">
+                                                        <input type="number" className="form-control" id="interval" name="interval" 
+                                                         onChange={(value) => this.setState({intervalo: value.target.value})}/>
                                                     </div>
-                                                </div>
+                                                </div>                                                
+                                                
                                                 <div className="form-group">
-                                                    <label htmlFor="date" className="col-sm-5 control-label">Data</label>
-                                                    <div className="col-sm-9">
-                                                        <input type="date" className="form-control" id="date" name="date" 
-                                                         onChange={(value) => this.setState({data: value.target.value})}/>
-                                                    </div>
-                                                </div>
-                                                <div className="form-group">
-                                                    <div className="col-sm-12 text-center">
-                                                        <Button type="button" size="sm" className="btn btn-default preview-add-button" 
-                                                            onClick={this.add}>
-                                                            <span className="glyphicon glyphicon-plus"></span> Adicionar
+                                                    <div className="col-sm-5 text-center">
+                                                        <Button outline type="button" size="sm" className="btn btn-default preview-add-button" 
+                                                            onClick={this.handleAdd}>
+                                                            <span  className="glyphicon glyphicon-plus"></span> Adicionar
                                                         </Button>
                                                     </div>
                                                 </div>
+                                                </form>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="col-sm-7">
-                                        
+                                    <div className="col-sm-6">                                        
                                         <div className="row">
-                                            <div className="col-xs-12">
+                                            <div className="">
                                                 <div className="table-responsive">
                                                     <table className="table preview-table">
                                                         <thead>
                                                             <tr>
-                                                                <th>Medicamento</th>
-                                                                <th>Descrição</th>
+                                                                <th>Medicamento</th>                                                                
+                                                                <th>Dose</th>
                                                                 <th>Quantidade</th>
-                                                                <th>Status</th>
-                                                                <th>Data</th>
+                                                                <th>Intervalo</th>
+                                                                <th></th>
+                                                                                                                               
                                                             </tr>
-                                                        </thead>
-                                                        <tbody>
-                                                            <tr>
-                                                                <th>1</th>
-                                                                <th>2</th>
-                                                                <th>3</th>
-                                                                <th>4</th>
-                                                                <th>5</th>
-                                                            </tr>
+                                                        </thead>                                                        
+                                                        <tbody> 
+                                                        { this.state.meds.map((medicamento) =>                                                                                                                      
+                                                            <tr key = {medicamento.nome}>
+                                                                <th>{medicamento.nome}</th>
+                                                                <th>{medicamento.dosagem}</th>                                                                
+                                                                <th>{medicamento.quantidade}</th>
+                                                                <th>{medicamento.intervalo}</th>                                                                
+                                                                <th><button type="button" class="close" aria-label="Close"
+                                                                        onClick={() => this.handleRemove(medicamento)}>
+                                                                        <span aria-hidden="true">&times;</span>
+                                                                        </button></th>
+                                                                
+                                                                    </tr>
+                                                        )}
                                                         </tbody>
                                                     </table>
                                                 </div>
@@ -134,7 +164,7 @@ class Prescription extends React.Component {
                                         </div>
                                         <div className="text-center">
                                             <div className="col-xs-12">
-                                            <Button size = "sm" color="success" rounded className="float-right" onClick={this.handleSubmission}>Feito!</Button>
+                                            <Button size = "sm" onClick={() => this.handlePrescription()}>Receitar!</Button>
                                             </div>
                                         </div> 
                                                                              
@@ -155,4 +185,4 @@ class Prescription extends React.Component {
     }
 }
 
-export default Prescription;
+export default withRouter(Prescription);
