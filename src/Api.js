f7e58f2964f5f9ca6e8a081ffa98ec800f8ce334
lucasduarte2 <lucas.duarte@ccc.ufcg.edu.br>
2018-11-02 18:29:27 -0300

    edit info de medico funcionando parcialmente

--- src/Api.js
+++ src/Api.js
@@ -8,8 +8,9 @@ const apis = {
      saveUser: (newUser) => api.post("users",newUser),
      loadUsers: () => api.get('users'),
      savePatient: (newPatient) => api.post("/paciente",newPatient),
-     loadPatients: () => api.get('paciente')
+     loadPatients: () => api.get('paciente'),
+     loadDoctor: () => api.get('medico'),
+     saveDoctor: (newDoctor) => api.put("/medico",newDoctor),
 }
-
 
 export default apis;
--- src/meuPerfil/MeuPerfil.js
+++ src/meuPerfil/MeuPerfil.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../Api';
 import './MeuPerfil.css';
 import EditDados from './EditDados.js';
-const MeuPerfil = (props) => {
-  return (
-      <section className="team-section text-center my-5">
-        <div className="jumbotron text-center">
-            <EditDados/>
-            <h2 className="card-title h2">Lucas venancio Duarte</h2>
-            <p className="blue-text my-4 font-weight-bold">espericalidade</p>
-            <div className="row d-flex justify-content-center">
-              <div className="col-xl-7 pb-2">
-                <p className="card-text">CRM</p>
-                <p className="card-text">Cpf</p>
-              </div>
-            </div>
-            <hr className="my-4"/>
-            <div className="pt-1">
-              <p className="card-text">Email</p>
-              <p className="card-text">Telefone</p>
-            </div>
-         </div>
-      </section>
-  );
-}
 
+class MeuPerfil extends Component {
+
+   state = {
+        medico: ""
+    }
+    componentDidMount() {
+        api.loadDoctor()
+        .then(res => {
+            const medico = res.data;
+            this.setState({ medico });
+        })
+    }
+
+  render() {
+    return (
+        <section className="team-section text-center my-5">
+          <div className="jumbotron text-center">
+              <EditDados nome={this.state.medico.nome}
+                          cpf={this.state.medico.cpf}
+                          crm={this.state.medico.crm}/>
+              <h2 className="card-title h2">{this.state.medico.nome}</h2>
+              <p className="text my-4">{this.state.medico.especialidade}</p>
+              <div className="row d-flex justify-content-center">
+                <div className="col-xl-7 pb-2">
+                  <p className="card-text">{this.state.medico.cpf}</p>
+                  <p className="card-text">{this.state.medico.crm}</p>
+                </div>
+              </div>
+              <hr className="my-4"/>
+              <div className="pt-1">
+                <p className="card-text">{this.state.medico.email}</p>
+                <p className="card-text">{this.state.medico.telefone}</p>
+              </div>
+           </div>
+        </section>
+    );
+  }
+}
 export default MeuPerfil;
