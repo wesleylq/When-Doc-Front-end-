2e708a9642c3658ce9d538d3406391ea5c02ac78
lucasduarte2 <lucas.duarte@ccc.ufcg.edu.br>
2018-11-10 09:56:38 -0300

    lista pacientes do medico funcionando

--- src/paciente/Paciente.js
+++ src/paciente/Paciente.js
@@ -4,19 +4,40 @@ import { Table, TableBody, TableHead  } from 'mdbreact'
 import ModalCadastraPaciente from './ModalCadastraPaciente'
 import api from '../Api';
 import ModalConsultaPaciente from './ModalConsultaPaciente'
+import Auth from "../login/Auth";
 
 class Paciente extends Component {
 
-    state = {
-        pacientes: []
-    }
+  constructor(props) {
+    super(props);
+        this.state = {
+             medico: "",
+             pacientes: []
+        }
+
+      }
+
     componentDidMount() {
-        api.loadPatients()
+        api.loadDoctor(Auth.getDoctor())
         .then(res => {
-            const pacientes = res.data;
-            this.setState({ pacientes });
+            const medico = res.data;
+            this.setState({ medico });
+            console.log(medico.crm);
+
+
+            api.loadPatients(medico.crm)
+            .then(res => {
+                const pacientes = res.data;
+                this.setState({ pacientes });
+                //console.log(pacientes);
+            })
+
+
         })
+
+
     }
+
 
       render() {
         return (
@@ -32,29 +53,30 @@ class Paciente extends Component {
                         </tr>
                     </TableHead>
                     <TableBody>
-                       { this.state.pacientes.map( (paciente,index) =>
-                            <tr key={index}>
-                                <td>{paciente.nome}</td>
-                                <td>{paciente.email}</td>
-                                <td>{paciente.cpf}</td>
-                                <td> <ModalConsultaPaciente
-                                                nome={paciente.nome}
-                                                cpf={paciente.cpf}
-                                                email={paciente.email}
-                                                emailSec={paciente.emailSec}
-                                                telefone={paciente.telefone}
-                                                telefoneSec={paciente.telefoneSec}
-                                                tipoSanguineo={paciente.tipoSanguineo}
-                                                rua={paciente.endereco.rua}
-                                                bairro={paciente.endereco.bairro}
-                                                numero={paciente.endereco.numero}
-                                                complemento={paciente.endereco.complemento}
-                                                cidade={paciente.endereco.cidade}
-                                                pais={paciente.endereco.pais}
-                                                cep={paciente.endereco.cep}
-                                                />
-                                </td>
-                            </tr>)}
+                    { this.state.pacientes.map( (paciente,index) =>
+                           <tr key={index}>
+                               <td>{paciente.nome}</td>
+                               <td>{paciente.email}</td>
+                               <td>{paciente.cpf}</td>
+                               <td> <ModalConsultaPaciente
+                                               nome={paciente.nome}
+                                               cpf={paciente.cpf}
+                                               email={paciente.email}
+                                               emailSec={paciente.emailSec}
+                                               telefone={paciente.telefone}
+                                               telefoneSec={paciente.telefoneSec}
+                                               tipoSanguineo={paciente.tipoSanguineo}
+                                               rua={paciente.endereco.rua}
+                                               bairro={paciente.endereco.bairro}
+                                               numero={paciente.endereco.numero}
+                                               complemento={paciente.endereco.complemento}
+                                               cidade={paciente.endereco.cidade}
+                                               pais={paciente.endereco.pais}
+                                               cep={paciente.endereco.cep}
+                                               />
+                               </td>
+                           </tr>)}
+
                     </TableBody>
                 </Table>
             </div>
